30b7a420ce95c40f1528100c9d5c652ad84023df
cafe <unptitkawa@gmail.com>
2020-12-12 23:19:17 +0100

    scraper facilities

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const prompts = require("prompts");
-const scraper = require("./src/scraper");
-
-const testModule = require("./src/scraper/browser_instance");
-testModule();
+const { scrap, Scraper, ScrapTask } = require("./src/scraper");
 
 const app = express();
 const port = 3000;
+const scraper = new Scraper();
 
 app.get("/", async (req, res) => {
 	console.log("start scrapping");
@@ -15,7 +13,28 @@ app.get("/", async (req, res) => {
 });
 
 app.listen(port, async () => {
+	await scraper.init();
+
 	console.log(`Example app listening at http://localhost:${port}`);
+
+	const data = await scraper.select_all(
+		"https://www.smashbros.com/en_US/fighter/index.html",
+		"li.fighter-list__item",
+		(elems) => {
+			return elems.map((elem) => {
+				const id = elem.querySelector("em.fighter-list__num-txt").innerText;
+				const name = elem.querySelector("p.fighter-list__name-main").innerText;
+				return {
+					id,
+					name,
+				};
+			});
+		},
+		(entry) => {
+			return entry.id && entry.name;
+		}
+	);
+	console.log("end scrap", data);
 	// const cmd = await prompts({
 	//   name: "cmd",
 	//   type: "confirm",
--- src/scraper/index.js
+++ src/scraper/index.js
@@ -1,11 +1,45 @@
 const puppeteer = require("puppeteer");
 
+class ScrapTask {
+	constructor({ url, task }) {
+		this.url = url ? url : "";
+		this.task = task ? task : () => true;
+	}
+}
+
+class Scraper {
+	constructor() {
+		this.browser = null;
+	}
+
+	async init() {
+		this.browser = await puppeteer.launch();
+	}
+
+	async scrap(url, fn) {
+		console.log("start scraping...");
+		const page = await this.browser.newPage();
+		await page.goto(url);
+		const result = await fn(page);
+		page.close();
+		return result;
+	}
+
+	async select_all(url, query, format, validate = () => true) {
+		const data = await this.scrap(url, async (page) => {
+			return page.$$eval(query, format);
+		});
+		return data.filter(validate);
+	}
+}
+
 async function scrap() {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 	await page.goto("https://www.smashbros.com/en_US/fighter/index.html");
 
-	const data = await page.evaluate(() => {
+	const data = await page.evaluate((test) => {
+		console.log(test);
 		const elems = document.querySelectorAll("li.fighter-list__item");
 		let formated_data = [];
 
@@ -18,11 +52,13 @@ async function scrap() {
 		}
 
 		return formated_data;
-	});
+	}, test);
 	console.log(data);
 	browser.close();
 }
 
 module.exports = {
 	scrap,
+	Scraper,
+	ScrapTask,
 };
