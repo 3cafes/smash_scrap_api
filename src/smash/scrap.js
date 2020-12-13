65a52577a6f43ccbc867a1ea7e6d176efe165f43
cafe <unptitkawa@gmail.com>
2020-12-13 11:11:39 +0100

    download player img blob

--- src/smash/scrap.js
+++ src/smash/scrap.js
@@ -1,18 +1,29 @@
-const scraper = require.main.require("./src/scraper");
+const scraper = require.main.require('./src/scraper');
 
-const BASE_URL = "https://www.smashbros.com";
+const BASE_URL = 'https://www.smashbros.com';
+
+async function download_players_img(players) {
+	return Promise.all(
+		players.map(async (player) => {
+			const img_blob = await scraper.scrap_image(
+				`${BASE_URL}${player.img_url}`
+			);
+			return { ...player, img_blob: img_blob };
+		})
+	);
+}
 
 async function scrap_players() {
 	const players = await scraper.scrap_page(
 		`${BASE_URL}/en_US/fighter/index.html`,
 		async (page) => {
-			const data = await page.$$eval("li.fighter-list__item", (elems) => {
+			const data = await page.$$eval('li.fighter-list__item', (elems) => {
 				return elems.map((elem) => {
-					const id = elem.querySelector("em.fighter-list__num-txt").innerText;
-					const name = elem.querySelector("p.fighter-list__name-main")
+					const id = elem.querySelector('em.fighter-list__num-txt').innerText;
+					const name = elem.querySelector('p.fighter-list__name-main')
 						.innerText;
 					const img_url = elem
-						.querySelector("div.fighter-list__img")
+						.querySelector('div.fighter-list__img')
 						.style.backgroundImage.split('"')[1];
 					return {
 						id,
@@ -24,8 +35,7 @@ async function scrap_players() {
 			return data.filter((entry) => entry.id && entry.name);
 		}
 	);
-	return players;
-	//return Promise.all(players.map(scrap_player_picture));
+	return download_players_img(players);
 }
 
 module.exports = {
